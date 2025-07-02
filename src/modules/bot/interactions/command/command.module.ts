59f74d4649760a3ba2c9d6bcfa1e06812875f61d
Tu 1 Dang Van Hoai <166920130+tudvh1@users.noreply.github.com>
2025-07-02 21:24:04 +0700

    feat: qtcs (#7)

--- src/modules/bot/interactions/command/command.module.ts
+++ src/modules/bot/interactions/command/command.module.ts
@@ -4,11 +4,11 @@ import { MenuModule } from '@/modules/menu/menu.module'
 import { MezonClientModule } from '@/modules/mezon-client/mezon-client.module'
 import { PuppeteerModule } from '@/modules/puppeteer/puppeteer.module'
 import { CommandDispatcher } from './command.dispatcher'
-import { MenuCommand, TruanayangiCommand } from './handlers'
+import { MenuCommand, QtcsCommand, TruanayangiCommand } from './handlers'
 
 @Module({
   imports: [MezonClientModule, PuppeteerModule, MenuModule],
-  providers: [CommandDispatcher, TruanayangiCommand, MenuCommand],
-  exports: [CommandDispatcher, TruanayangiCommand, MenuCommand],
+  providers: [CommandDispatcher, TruanayangiCommand, MenuCommand, QtcsCommand],
+  exports: [CommandDispatcher, TruanayangiCommand],
 })
 export class CommandModule {}
